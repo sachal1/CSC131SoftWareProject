2048fe730610ebfd3b61c899c80c8afbea76ad86
Eduardo Lopez <eduardolopez652@yahoo.com>
2022-10-17 23:43:33 -0700

    Testing hope this works

---
+++ Test Ed push/venv/app.py
@@ -0,0 +1,7 @@
+#Test for add, commit and push
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return 'Hello Team hope everyone is doing well and having a good one'
