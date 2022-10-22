d2cc4ca460d49f05f6afba0fd6109b32b2513b4d
dinhphucv <90453629+dinhphucv@users.noreply.github.com>
2022-10-22 00:49:14 -0700

    Create movie.py with basic variables

---
+++ movie.py
@@ -0,0 +1,8 @@
+class Movie:
+
+    def __int__(self, title, year, directors, poster, imdb_id):
+        self.title = title
+        self.year = year
+        self.directors = directors
+        self.poster = poster
+        self.imdb_id = imdb_id
