6bb18955b49920bea87e5d97ab6bc2a268cfe8ec
CesarMDC4 <cesarmartindelcampo@csus.edu>
2022-10-24 15:14:10 -0700

    demo push

--- test/venv/app.py
+++ test/venv/app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+# from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
-@app.route('/')
-def welcome():
-    return 'Hello World!!!'
+HOST = "127.0.0.1"
+PORT = 8080
+
+
+engine = create_engine('mysql+pymysql://root:123456@127.0.0.1/movie_db')
+
+
+@app.route("/hello-world")
+def hello_world():
+    return "Hello, World!"
+
+
+if __name__ == '__main__':
+    app.run(host=HOST, port=PORT)
