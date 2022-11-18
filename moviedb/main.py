200fbb1eec71226488ae06450821635a6264d50b
dinhphucv <90453629+dinhphucv@users.noreply.github.com>
2022-11-18 11:27:05 -0800

    Further improvements and clean some of reuse code with function

--- moviedb/main.py
+++ moviedb/main.py
@@ -9,17 +9,11 @@ from flask import flash, json, jsonify, request
 HOST = "127.0.0.1"
 PORT = 8080
 OMDB_API_KEY = "c37e6c14"
-omdb_url = f"https://www.omdbapi.com/?apikey={OMDB_API_KEY}"
+OMDB_URL = f"https://www.omdbapi.com/?apikey={OMDB_API_KEY}"
 
 
-@app.route("/hello-world")
-def hello_world():
-    return "Hello, World!"
-
-
-# This function take a json as parameter and add it to the movies table.
-def import_movie_from_omdb(response):
-    _json = response
+def import_movie_from_json(original_movie_data):
+    _json = original_movie_data
     _title = _json["Title"]
     _year = _json["Year"]
     _directors = _json["Director"]
@@ -31,19 +25,18 @@ def import_movie_from_omdb(response):
     _imdb_id = _json["imdbID"]
     conn = mysql.connect()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
-    sqlQuery = "INSERT INTO movies(title, year, directors, genre, actors, language, awards, poster, imdb_id) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    bindData = (_title, _year, _directors, _genre, _actors, _language, _awards, _poster, _imdb_id)
-    cursor.execute(sqlQuery, bindData)
+    sql_query = "INSERT INTO movies(title, year, directors, genre, actors, language, awards, poster, imdb_id) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    bind_data = (_title, _year, _directors, _genre, _actors, _language, _awards, _poster, _imdb_id)
+    cursor.execute(sql_query, bind_data)
     conn.commit()
+    cursor.execute("SELECT id, title, year, directors, genre, actors, language, awards, poster, imdb_id FROM movies WHERE imdb_id = %s", _imdb_id)
+    movie_data = cursor.fetchone()
     cursor.close()
     conn.close()
-    response = jsonify('Test')
-    response.status_code = 200
-    return response
+    return movie_data
 
 
-# This method checking if the movie is already exits in the database using imdbID as parameter
-def is_movie_exist_in_database(imdb_id):
+def is_imdb_id_existed_in_database(imdb_id):
     conn = mysql.connect()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
     cursor.execute("SELECT id, title, year, directors, genre, actors, language, awards, poster, imdb_id FROM movies WHERE imdb_id = %s", imdb_id)
@@ -54,28 +47,53 @@ def is_movie_exist_in_database(imdb_id):
         return jsonify(movie_data)
 
 
-# This method take movie title (and year) and parameter and return its data
+def is_title_existed_in_database(title):
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    cursor.execute("SELECT id, title, year, directors, genre, actors, language, awards, poster, imdb_id FROM movies WHERE title = %s", title)
+    movie_data = cursor.fetchone()
+    if movie_data is None:
+        return 0
+    else:
+        return jsonify(movie_data)
+
+
+def get_imdb_id_from_json(movie_data):
+    return movie_data["imdbID"]
+
+
+def get_movie_data_from_omdb_with_title(movie_title):
+    return requests.get(OMDB_URL + "&t=" + movie_title).json()
+
+
+def get_movie_data_from_omdb_with_title_year(movie_title, year):
+    return requests.get(OMDB_URL + "&t=" + movie_title + "&y=" + year).json()
+
+
+def get_movie_data_from_omdb_with_imdb_id(imdb_id):
+    return requests.get(OMDB_URL + "&i=" + imdb_id).json()
+
+
+def append_movie_data_to_a_json(movie_tile):
+    return 0
+
+
 @app.route("/api/v1/search")
-def get_movie_from_omdb():
-    global response
-    #omdb_url = f"https://www.omdbapi.com/?apikey={OMDB_API_KEY}"
+def search():
     if "title" and "year" in request.args:
-        response = requests.get(omdb_url + "&t=" + request.args["title"] + "&y=" + request.args["year"]).json()
-    elif "title" in request.args:
-        response = requests.get(omdb_url + "&t=" + request.args["title"]).json()
+        response = get_movie_data_from_omdb_with_title_year(request.args["title"], request.args["year"])
+    if "title" in request.args:
+        response = get_movie_data_from_omdb_with_title(request.args["title"])
     imdb_id = response["imdbID"]
-    movie_data = is_movie_exist_in_database(imdb_id)
-    # Need further implementation to checking if the imdbID is already in the database
+    movie_data = is_imdb_id_existed_in_database(imdb_id)
     if movie_data == 0:
-        import_movie_from_omdb(response)
+        return import_movie_from_json(response)
     else:
         return movie_data
-    return response
 
 
-# This method take the movie_id as parameter, searching through MySQL and return data of matching movie
 @app.get("/api/v1/movies/<int:movie_id>")
-def get_movie_from_database(movie_id: int):
+def get_movie_data_from_database(movie_id: int):
     conn = mysql.connect()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
     cursor.execute("SELECT id, title, year, directors, genre, actors, language, awards, poster, imdb_id FROM movies WHERE id = %s", movie_id)
@@ -83,7 +101,7 @@ def get_movie_from_database(movie_id: int):
     cursor.close()
     conn.close()
     if movie_data is None:
-        return {"error": f"No movie found for ID {movie_id}"}, 404
+        return {"error": f"no movie found for id ({movie_id})"}, 404
     else:
         return movie_data, 200
 
@@ -91,40 +109,29 @@ def get_movie_from_database(movie_id: int):
 @app.post("/api/v1/movies")
 def create_movie():
     if request.is_json:
-        _json = request.json
-        _title = _json["Title"]
-        _year = _json["Year"]
-        _directors = _json["Director"]
-        _genre = _json["Genre"]
-        _actors = _json["Actors"]
-        _language = _json["Language"]
-        _awards = _json["Awards"]
-        _poster = _json["Poster"]
-        _imdb_id = _json["imdbID"]
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        sqlQuery = "INSERT INTO movies(title, year, directors, genre, actors, language, awards, poster, imdb_id) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        bindData = (_title, _year, _directors, _genre, _actors, _language, _awards, _poster, _imdb_id)
-        cursor.execute(sqlQuery, bindData)
-        conn.commit()
-        cursor.close()
-        conn.close()
+        if "imdbID" in request.json:
+            imdb_id = request.json.get("imdbID")
+            movie_data = is_imdb_id_existed_in_database(imdb_id)
+            if movie_data == 0:
+                return import_movie_from_json(get_movie_data_from_omdb_with_imdb_id(imdb_id))
+            else:
+                return {"error": f"movie with IMDb ({imdb_id}) already existed in the database"}, 200
+
+
+@app.put("/api/v1/movies/<int:movie_id>")
+def update_movie_data_in_database(movie_id: int):
 
 
 # This method takes movie_id as parameter and delete the movie matched that id.
 @app.delete("/api/v1/movies/<int:movie_id>")
 def delete_movie(movie_id: int):
-    try:
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        cursor.execute("DELETE FROM movies WHERE id = %s", movie_id)
-        conn.commit()
-        return {"success": f"Movie {movie_id} has been deleted from the database"}, 200
-    except Exception as e:
-        print(e)
-    finally:
-        cursor.close()
-        conn.close()
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM movies WHERE id = %s", movie_id)
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return {"success": f"movie {movie_id} has been deleted from the database"}, 200
 
 
 # This function will return all academy awards nominees
@@ -137,6 +144,8 @@ def get_academy_awards_nominees(year: int):
         for row in reader:
             if int(row[1]) == year:
                 result.append(dict(zip(header, row)))
+    if not result:
+        return {"error": f"no data found for this year ({year})"}, 404
     return json.dumps(result)
 
 
@@ -148,20 +157,34 @@ def get_academy_awards_best_picture_winner(year: int):
         for row in reader:
             if int(row[1]) == year and row[6] == "True" and (row[3] == "OUTSTANDING PICTURE" or row[3] == "BEST PICTURE"):
                 movie_title = row[5]
-    response = requests.get(omdb_url + "&t=" + movie_title).json()
-    return response
+                data = dict(zip(header, row))
+                movie_data = is_title_existed_in_database(movie_title)
+                if movie_data == 0:
+                    movie_data = import_movie_from_json(get_movie_data_from_omdb_with_title(movie_title))
+                    data["movie_data"] = movie_data
+                else:
+                    data["movie_data"] = movie_data.json
+    return jsonify(data)
 
 
 @app.get("/api/v1/academy-awards/best-actors/<int:year>")
 def get_academy_awards_best_actor_winner(year: int):
     with open("csvs/the_oscar_award.csv") as f:
+        result = []
         reader = csv.reader(f)
         header = next(reader)
         for row in reader:
-            if int(row[1]) == year and row[6] == "True" and row[3] == "ACTOR":
+            if int(row[1]) == year and row[6] == "True" and (row[3] == "ACTOR" or row[3] == "ACTRESS" or row[3] == "ACTOR IN A LEADING ROLE" or row[3] == "ACTRESS IN A LEADING ROLE"):
                 movie_title = row[5]
-    response = requests.get(omdb_url + "&t=" + movie_title).json()
-    return response
+                data = dict(zip(header, row))
+                movie_data = is_title_existed_in_database(movie_title)
+                if movie_data == 0:
+                    movie_data = import_movie_from_json(get_movie_data_from_omdb_with_title(movie_title))
+                    data["movie_data"] = movie_data
+                else:
+                    data["movie_data"] = movie_data.json
+                result.append(data)
+    return jsonify(result)
 
 
 if __name__ == '__main__':
